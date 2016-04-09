9e8b4291bdc41bab86c429dc417347225f14b21e
Josh Graber <jgraber@renovo1.com>
2016-04-09 12:12:35 -0400

    Add karma as a dependency, add karma configuration. Add typings for mocha and chai so that we can write tests in Typescript.

---
+++ karma.config.js
@@ -0,0 +1,73 @@
+// Karma configuration
+// Generated on Sat Apr 09 2016 11:23:55 GMT-0400 (Eastern Daylight Time)
+
+module.exports = function(config) {
+	var karmaSettings = {
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
+
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai'],
+
+		// list of files / patterns to load in the browser
+		files: [
+			'node_modules/angular/angular.min.js',
+			'node_modules/angular-mocks/angular-mocks.js',
+			'built/**/*.tests.js'
+		],
+
+		preprocessors: {
+			'built/**/*.tests.js': ['webpack'],
+		},
+
+		webpack: {
+			module: {
+				loaders: [
+					{
+						test: /\.html$/,
+						loader: 'text',
+					},
+				],
+			},
+			externals: [
+				{
+					angular: 'angular',
+				},
+			],
+			bail: true,
+			devtool: 'inline-source-map',
+		},
+
+		browsers: ['Chrome', 'Firefox'],
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
+
+		// web server port
+		port: 9876,
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity,
+	}
+
+	config.set(karmaSettings)
+	return karmaSettings
+}
